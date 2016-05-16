15a7e966096a6c5c516ba93e860af8d7824ba67c
Xiaonan Tong <tongxn2009@gmail.com>
2016-05-16 02:05:17 -0700

    Added assets for key highlighting as well as code for key highlighting and describing the piano.

---
+++ Pianolens/Assets/HighlightEveryKey.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightEveryKey : MonoBehaviour {
+    bool[] keyIsHighlighted = new bool[109];
+    private PianoDescriptor p;
+    private Vector3 start;
+    private Vector3 end;
+
+	// Use this for initialization
+	void Start () {
+        p = PianoDescriptor.getPianoDescriptor();
+        GameObject leftAnchor = GameObject.Find("leftAnchor");
+        GameObject rightAnchor = GameObject.Find("rightAnchor");
+        print(rightAnchor);
+
+        start = leftAnchor.transform.position;
+        end = rightAnchor.transform.position;
+        print(start);
+        print(end);
+
+        Vector3 length = end - start;
+        Vector3 lengthCentered = length * ((float)p.NumKeys / (p.NumKeys - 1));
+        print(lengthCentered);
+        length = start + lengthCentered;
+        //try to extend from start to end, but not reach end.
+        Vector3 halflength = (end - length) / (float)2.0;
+
+
+        end = halflength + length;
+        start = start + halflength;
+        print(end);
+        print(start);
+
+        for(int i = 0; i < keyIsHighlighted.Length; i++)
+        {
+            keyIsHighlighted[i] = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        int i = Random.Range(21, 109);
+        if (keyIsHighlighted[i]) {
+            GameObject g = GameObject.Find("highlighting" + i);
+            GameObject.DestroyObject(g);
+            keyIsHighlighted[i] = false;
+        }
+        else
+        {
+            Vector3[] pos = p.getKeyWidth(i, false, ref start, ref end);
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            //print(pos[0]);
+            //print(pos[1]);
+            cube.name = "highlighting" + i;
+            cube.transform.position = (pos[0]);
+            //print(pos[0]);
+            cube.transform.localScale = new Vector3(0.003f, 0.001f, 0.05f);
+            //GameObject.Add
+            keyIsHighlighted[i] = true;
+
+        }
+    }
+}
---
+++ Pianolens/Assets/PianoDescriptor.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PianoDescriptor {
+    int numOfScales = 7;
+    int numKeys = 7*12+4;
+    int numWhiteKeys = 52;
+
+    private static PianoDescriptor descriptor;
+
+    public int NumOfScales
+    {
+        get
+        {
+            return numOfScales;
+        }
+
+        set
+        {
+            numOfScales = value;
+        }
+    }
+
+    public int NumKeys
+    {
+        get
+        {
+            return numKeys;
+        }
+
+        set
+        {
+            numKeys = value;
+        }
+    }
+
+    public int NumWhiteKeys
+    {
+        get
+        {
+            return numWhiteKeys;
+        }
+
+        set
+        {
+            numWhiteKeys = value;
+        }
+    }
+
+    /// <summary>
+    /// Static function return a Piano Descriptor singleton.
+    /// </summary>
+    /// <returns></returns>
+    public static PianoDescriptor getPianoDescriptor()
+    {
+        if (descriptor == null) descriptor = new PianoDescriptor();
+        return descriptor;
+    }
+
+    /// <summary>
+    /// Currently assumes a piano starting on A0 and Ending on C8(id 108).
+    /// </summary>
+    public PianoDescriptor()
+    {
+        NumKeys = 7*12+4;
+        NumOfScales = 7;
+    }
+
+    public PianoDescriptor(int numOfScales)
+    {
+        this.NumKeys = numOfScales*12 + 4;
+        this.NumOfScales = numOfScales;
+    }
+
+    //http://computermusicresource.com/midikeys.html
+    //Note 60 is the central C3, and others are around it.
+    //the lowest note on an 88 is A. i.e. 21. 
+    //Lowest C is the 24.
+    //highest note on an 88 is a 108 (C8)
+    /// <summary>
+    /// Gets the key width given two vector references denoting the absolute position of the center of the first and last key on the piano.
+    /// </summary>
+    /// <param name="keyId">Absolute Key ID as used by MIDI standard on http://computermusicresource.com/midikeys.html </param>
+    /// <param name="isWhite">If is white, then we return the full width fo the white key, otherwise we just use a standard key width.</param>
+    /// <param name="start">Absolute Start 3D vector position for the first key (center of key)</param>
+    /// <param name="end">Absolute start 3D vector position for the last key (center)</param>
+    /// <returns></returns>
+    public Vector3[] getKeyWidth(int keyId, bool isWhite, ref Vector3 start, ref Vector3 end)
+    {
+        Vector3[] keySpace = new Vector3[2];
+        Vector3 absolutePosition = (end - start)*((float)(keyId-21-1) / (float)(108-21-1)) ;
+        //Fixme: the -1 might be right or might be wrong. Testing is needed.
+        if (isWhite)
+        {
+            Vector3 perWhiteKeyWidth = (end - start) / (float)NumWhiteKeys;
+            keySpace[0] = absolutePosition - perWhiteKeyWidth + start;
+            keySpace[1] = absolutePosition + perWhiteKeyWidth + start;
+        }
+        else
+        {
+            Vector3 perKeyWidth = (end - start) / (float)NumKeys;
+            keySpace[0] = absolutePosition - perKeyWidth + start;
+            keySpace[1] = absolutePosition + perKeyWidth + start;
+        }
+        
+        return keySpace;
+    }
+
+
+}
