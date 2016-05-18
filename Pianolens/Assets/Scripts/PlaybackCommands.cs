20a4e0aa14d340f28a6c9aeaba3f44d7d4fff433
Evan Strasnick <estrasnick@gmail.com>
2016-05-18 00:26:39 -0700

    Created a rudimentary interface in the world with four buttons, which respond to gaze+tap by changing color.

---
+++ Pianolens/Assets/Scripts/PlaybackCommands.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlaybackCommands : MonoBehaviour
+{
+    Vector3 originalPosition;
+    bool IsPaused = false;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+
+        switch (this.name)
+        {
+            case "PlayPause":
+                if (IsPaused)
+                {
+                    renderer.material.color = new Color(255, 255, 255);
+                }
+                else
+                {
+                    renderer.material.color = new Color(0, 0, 0);
+                }
+                IsPaused = !IsPaused;
+                break;
+            case "FastForward":
+                renderer.material.color = new Color(255, 0, 0);
+                break;
+            case "Rewind":
+                renderer.material.color = new Color(0, 255, 0);
+                break;
+            case "Record":
+                renderer.material.color = new Color(0, 0, 255);
+                break;
+            default:
+                renderer.material.color = new Color(0, 0, 0);
+                break;
+        }
+        
+        
+    }
+}
