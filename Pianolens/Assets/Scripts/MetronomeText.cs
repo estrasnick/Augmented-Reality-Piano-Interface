ca439d8a8aa849f5e03525cade7f48fb75a9875a
Evan Strasnick <estrasnick@gmail.com>
2016-05-18 19:23:41 -0700

    Added a dynamic BPM display, which can be incremented or decremented via buttons on the side.

---
+++ Pianolens/Assets/Scripts/MetronomeText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MetronomeText : MonoBehaviour {
+
+    int BPM;
+    Text text;
+
+	// Use this for initialization
+	void Start () {
+        BPM = 100;
+        text = this.GetComponent<Text>();
+        Update();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        text.text = BPM + "\nBPM";
+    }
+
+	void IncrementBPM () {
+        BPM++;
+	}
+
+    void DecrementBPM()
+    {
+        if (BPM > 0)
+        {
+            BPM--;
+        }
+    }
+}
--- Pianolens/Assets/Scripts/PlaybackCommands.cs
+++ Pianolens/Assets/Scripts/PlaybackCommands.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaybackCommands : MonoBehaviour
 {
     Vector3 originalPosition;
     bool IsPaused = false;
+    GameObject BPMDisplay;
 
     // Use this for initialization
     void Start()
     {
+        BPMDisplay = GameObject.Find("BPMText");
     }
 
     // Called by GazeGestureManager when the user performs a Select gesture
@@ -36,6 +39,12 @@ public class PlaybackCommands : MonoBehaviour
                 break;
             case "Record":
                 renderer.material.color = new Color(0, 0, 255);
+                break;
+            case "BPMUp":
+                BPMDisplay.SendMessage("IncrementBPM"); 
+                break;
+            case "BPMDown":
+                BPMDisplay.SendMessage("DecrementBPM");
                 break;
             default:
                 renderer.material.color = new Color(0, 0, 0);
