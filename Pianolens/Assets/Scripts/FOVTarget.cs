bdc523d44fb5b812dd873a9815630ac76133dd74
austinchambers <austin@intapp.com>
2016-05-19 23:04:25 -0700

    #8 - Show left/right indicator if object is out of FOV.

--- Pianolens/Assets/Scripts/FOVTarget.cs
+++ Pianolens/Assets/Scripts/FOVTarget.cs
@@ -6,18 +6,29 @@ public class FOVTarget : MonoBehaviour
 {
     public Transform target;
     Camera FOVCamera;
+    private MeshRenderer[] leftMeshRenderer;
+    private MeshRenderer rightMeshRenderer;
 
     void Start()
     {
         FOVCamera = Camera.main;
+
+        //Grab the mesh renderer that's on the same object as this script.
+        leftMeshRenderer = FOVCamera.GetComponentsInChildren<MeshRenderer>();
     }
 
     void Update()
     {
         Vector3 viewPos = FOVCamera.WorldToViewportPoint(target.position);
         if (viewPos.x > 0.5F)
-            print("target is on the right side!");
+        {
+            leftMeshRenderer[0].enabled = false;
+            leftMeshRenderer[1].enabled = true;
+        }
         else
-            print("target is on the left side!");
+        {
+            leftMeshRenderer[0].enabled = true;
+            leftMeshRenderer[1].enabled = false;
+        }
     }
 }
