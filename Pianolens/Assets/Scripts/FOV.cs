bb0e60510200b9e64924ba02d783f8373f5b0a2f
austinchambers <austin@intapp.com>
2016-05-19 22:04:15 -0700

    #8 - Adjusted camera settings, near clip reduce and simple console logging to detect FOV relative to sphere 1

---
+++ Pianolens/Assets/Scripts/FOV.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FOV : MonoBehaviour
+{
+
+    private MeshRenderer[] meshRenderers;
+
+    // Use this for initialization
+    void Start()
+    {
+        //GameObject theObject = GameObject.Find("CursorTopLeft");
+        //Grab the mesh renderer within the object 
+        //theObject.transform.position = new Vector3(0.2f, 0.2f, 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ////Do a raycast into the world based on the user's 
+        ////head position and orientation.
+        //var headPosition = Camera.main.transform.position;
+        //var gazeDirection = Camera.main.transform.forward;
+
+        //RaycastHit hitInfo;
+
+        //if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        //{
+        //    // If the raycast hit a hologram...
+        //    // Display the cursor mesh.
+        //    meshRenderer.enabled = true;
+
+        //    // Move theCursor to the point where the raycase hit. 
+        //    this.transform.position = hitInfo.point;
+
+        //    // Rotate the cursor to hug the surface of the hologram
+        //    this.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+        //}
+        //else
+        //{
+        //    // If the raycast did not hit a hologram, hide the cursor mesh. 
+        //    meshRenderer.enabled = false;
+        //}
+    }
+}
---
+++ Pianolens/Assets/Scripts/FOVTarget.cs
@@ -0,0 +1,23 @@
+﻿// Finds out whether target is on the left or right side of the screen
+using UnityEngine;
+using System.Collections;
+
+public class FOVTarget : MonoBehaviour
+{
+    public Transform target;
+    Camera FOVCamera;
+
+    void Start()
+    {
+        FOVCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 viewPos = FOVCamera.WorldToViewportPoint(target.position);
+        if (viewPos.x > 0.5F)
+            print("target is on the right side!");
+        else
+            print("target is on the left side!");
+    }
+}
