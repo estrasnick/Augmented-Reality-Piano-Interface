e1acd66515cd873edd6d718d758a93c3c946de68
Evan Strasnick <estrasnick@gmail.com>
2016-05-25 02:40:58 -0700

    Implemented dynamic placement of notes given a specified song. Only a few of the supported notes are currently implemented in the switch statements.

--- Pianolens/Assets/Scripts/SheetMusicCycle.cs
+++ Pianolens/Assets/Scripts/SheetMusicCycle.cs
@@ -1,48 +1,132 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class SheetMusicCycle : MonoBehaviour {
 
-    static float Z_OFFSET = .05f;
+    static float BETWEEN_STAFF_LINE_DISTANCE = .0125f;
+    static float LINE_OFFSET = .0125f;
 
-    bool flipper;
+    static Bar firstBar = new Bar(new List<Note>() { new Note(1, 63, 1), new Note(2, 62, 1), new Note(3, 61, 1), new Note(4, 62, 1) });
+    static Bar secondBar = new Bar(new List<Note>() { new Note(1, 63, 1), new Note(2, 63, 1), new Note(3, 63, 2) });
+
+    static Song song = new Song("Mary Had a Little Lamb", "I Don't Know", (int)Keys.cmaj, 4, 4, new List<Bar>() { firstBar, secondBar }, 100);
+
     public Transform quarter_note;
     public Transform whole_note;
+    public Transform half_note;
 
     GameObject MusicSheet;
     Vector3 MusicSheetLoc;
 
-    // Use this for initialization
-    void Start () {
-        StartCoroutine(NextBar());
+    int currentBar;
 
+    // Use this for initialization
+    void Start() {
         MusicSheet = GameObject.Find("MusicSheet");
         MusicSheetLoc = MusicSheet.transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+
+        currentBar = 0;
+        StartCoroutine(NextBar());
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     IEnumerator NextBar()
     {
-        while(true)
+        while (true)
         {
             ClearBar();
+            RenderBar(song.GetBars()[currentBar++]);
+            if (currentBar >= song.GetBars().Count)
+            {
+                currentBar = 0;
+            }
 
-            Instantiate(flipper ? quarter_note : whole_note, new Vector3(MusicSheetLoc.x, MusicSheetLoc.y, MusicSheetLoc.z - Z_OFFSET), Quaternion.identity);
-            flipper = !flipper;
-            yield return new WaitForSeconds(2);
+            yield return new WaitForSeconds(3);
         }
     }
 
     void ClearBar()
     {
-        GameObject[] notes = GameObject.FindGameObjectsWithTag("MicroNote");
-        for (int i = 0; i < notes.Length; i++)
+        GameObject[] currentNotes = GameObject.FindGameObjectsWithTag("MicroNote");
+        for (int i = 0; i < currentNotes.Length; i++)
         {
-            Destroy(notes[i]);
+            Destroy(currentNotes[i]);
         }
+    }
+
+    void RenderBar(Bar bar)
+    {
+        foreach (Note note in bar.GetNotes())
+        {
+            Instantiate(getTransformForNoteRatio(note), getNotePosition(note), Quaternion.identity);
+        }
+    }
+
+    Transform getTransformForNoteRatio(Note note)
+    {
+        float ratio = (float) note.GetDuration() / song.GetBeatsPerMeasure();
+        if (ratio == .25f)
+        {
+            return quarter_note;
+        }
+        else if (ratio == .5f)
+        {
+            return half_note;
+        }
+        else if (ratio == 1)
+        {
+            return whole_note;
+        }
+        else
+        {
+            return quarter_note;
+        }
+    }
+
+    Vector3 getNotePosition(Note note)
+    {
+        float x = 0;
+        try
+        {
+            x = (MusicSheet.GetComponent<MeshFilter>().sharedMesh.bounds.size.x * .6f) * ((float) note.GetStartingBeat() / (song.GetBeatsPerMeasure() + 1)) + MusicSheetLoc.x - (.3f * MusicSheet.GetComponent<MeshFilter>().sharedMesh.bounds.size.x);
+        } catch (Exception e)
+        {
+            Debug.Log("X");
+        }
+
+        float y = 0;
+        try
+        {
+            switch (note.GetPianoKey())
+            {
+                case 61:
+                    y = GameObject.Find("StaffLine5").transform.position.y - BETWEEN_STAFF_LINE_DISTANCE - LINE_OFFSET;
+                    break;
+                case 62:
+                    y = GameObject.Find("StaffLine5").transform.position.y - LINE_OFFSET;
+                    break;
+                case 63:
+                    y = GameObject.Find("StaffLine5").transform.position.y + BETWEEN_STAFF_LINE_DISTANCE - LINE_OFFSET;
+                    break;
+                default:
+                    y = GameObject.Find("StaffLine5").transform.position.y - LINE_OFFSET;
+                    break;
+            }
+        } catch (Exception e)
+        {
+            Debug.Log("Y");
+        }
+
+float z = GameObject.Find("StaffLine5").transform.position.z;
+
+        Debug.Log("X: " + x + ", Y: " + y + ", Z: " + z);
+
+        return new Vector3(x, y, z);
     }
 }
---
+++ Pianolens/Assets/Scripts/Song.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public enum Keys
+{
+    amaj,
+    amin,
+    bmaj,
+    bmin,
+    cmaj,
+    cmin,
+    dmaj,
+    dmin,
+    emaj,
+    emin,
+    fmaj,
+    fmin,
+    gmaj,
+    gmin,
+    aflatmaj,
+    bflatmaj,
+    dflatmaj,
+    eflatmaj,
+    gflatmaj,
+    bflatmin,
+    csharpmin,
+    dsharpmin,
+    fsharpmin,
+    gsharpmin
+}
+
+public class Song
+{
+    string Title;
+    string Composer;
+    int Key;
+    int BeatsPerMeasure;
+    int BeatUnit;
+    List<Bar> Bars;
+    int DefaultBPM;
+    int CurrentBPM;
+
+    public Song(string title, string composer, int key, int beatsPerMeasure, int beatUnit, List<Bar> bars, int defaultBPM, int currentBPM = -1)
+    {
+        if (currentBPM == -1)
+        {
+            CurrentBPM = defaultBPM;
+        }
+        Title = title;
+        Composer = composer;
+        Key = key;
+        BeatsPerMeasure = beatsPerMeasure;
+        BeatUnit = beatUnit;
+        Bars = bars;
+        DefaultBPM = defaultBPM;
+    }
+
+    public string GetTitle()
+    {
+        return Title;
+    }
+
+    public string GetComposer()
+    {
+        return Composer;
+    }
+
+    public int GetKey()
+    {
+        return Key;
+    }
+
+    public int GetBeatsPerMeasure()
+    {
+        return BeatsPerMeasure;
+    }
+
+    public int GetBeatUnit()
+    {
+        return BeatUnit;
+    }
+
+    public int GetDefaultBPM()
+    {
+        return DefaultBPM;
+    }
+
+    public int GetCurrentBPM()
+    {
+        return CurrentBPM;
+    }
+
+    public List<Bar> GetBars()
+    {
+        return Bars;
+    }
+}
+
+public class Bar
+{
+    List<Note> Notes;
+
+    public Bar(List<Note> notes)
+    {
+        Notes = notes;
+    }
+
+    public List<Note> GetNotes()
+    {
+        return Notes;
+    }
+}
+
+public class Note
+{
+    static int REST = 0;
+
+    int StartingBeat;
+    int PianoKey; // # of the key on the piano. 0 indicates a rest.
+    int Duration; // duration, in beats
+
+    public Note(int startingBeat, int pianoKey, int duration)
+    {
+        StartingBeat = startingBeat;
+        PianoKey = pianoKey;
+        Duration = duration;
+    }
+
+    public int GetStartingBeat()
+    {
+        return StartingBeat;
+    }
+
+    public int GetPianoKey()
+    {
+        return PianoKey;
+    }
+
+    public int GetDuration()
+    {
+        return Duration;
+    }
+}
