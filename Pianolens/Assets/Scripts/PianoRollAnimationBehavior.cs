ac26066420ed43fdeb05ba087d449612971bd69d
Xiaonan Tong <tongxn2009@gmail.com>
2016-05-30 23:00:21 -0700

    some unfinished animation behavior.

---
+++ Pianolens/Assets/Scripts/PianoRollAnimationBehavior.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PianoRollAnimationBehavior : MonoBehaviour {
+
+    int measureToEndOn;
+    float currentMeasure;
+    void setup(int measureToEndOn, float currentMeasure)
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+        LeanTween.move(gameObject, getTarget(), timeLeft);
+	}
+
+}
