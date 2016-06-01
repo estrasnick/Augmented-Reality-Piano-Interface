9f97c484a9279527180fa0d826fe62890132909d
Evan Strasnick <estrasnick@gmail.com>
2016-05-31 21:14:10 -0700

    Added MIDI input over UDP. A corresponding python script should be used on a device reading midi input to send over UDP.

---
+++ Pianolens/Assets/Scripts/ReceiveUDP.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+#if !UNITY_EDITOR
+using Windows.Networking.Sockets;
+#endif
+
+public class ReceiveUDP : MonoBehaviour
+{
+    List<String> messages;
+
+#if !UNITY_EDITOR
+    DatagramSocket socket;
+#endif
+    // use this for initialization
+    async void Start()
+    {
+#if !UNITY_EDITOR
+        messages = new List<String>();
+
+        Debug.Log("Waiting for a connection...");
+
+        socket = new DatagramSocket();
+        socket.MessageReceived += Socket_MessageReceived;
+
+        try
+        {
+            await socket.BindEndpointAsync(null, "12345");
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.ToString());
+            Debug.Log(SocketError.GetStatus(e.HResult).ToString());
+            return;
+        }
+
+        Debug.Log("exit start");
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (String message in messages)
+        {
+            GameObject metronome = GameObject.Find("MetronomeInteractive");
+            if (message.Contains("55,"))
+            {
+                metronome.SendMessageUpwards("OnSelectDown");
+            }
+            else if (message.Contains("57,"))
+            {
+                metronome.SendMessageUpwards("OnSelectUp");
+            }
+        }
+        messages.Clear();
+    }
+
+#if !UNITY_EDITOR
+    private async void Socket_MessageReceived(Windows.Networking.Sockets.DatagramSocket sender,
+        Windows.Networking.Sockets.DatagramSocketMessageReceivedEventArgs args)
+    {
+        Debug.Log("Triggering new message!");
+        //Read the message that was received from the UDP echo client.
+        Stream streamIn = args.GetDataStream().AsStreamForRead();
+        StreamReader reader = new StreamReader(streamIn);
+        string message = await reader.ReadLineAsync();
+
+        messages.Add(message);
+        Debug.Log("MESSAGE: " + message);
+        
+        
+    }
+#endif
+}
